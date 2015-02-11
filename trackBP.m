f4c59d15a6f9e6db4a1a2b0f7ec58fbc7252d75a
nbush257 <nbush257@gmail.com>
2015-02-11 10:08:11 -0600

    Now using weighted centroid to find the basepoint. Going to start a new branch which finds the lowest intens near the center,

--- trackBP.m
+++ trackBP.m
@@ -10,13 +10,44 @@ I = v.getframe();
 I = histeq(I);
 imshow(I);
 zoom on;
-pause
+title('Zoom, then click on the basepoint');
+pause;
+
 bp = round(ginput(1));
 bpx = bp(1);
 bpy = bp(2);
 se = strel('square',10);% i fthis is odd, the for loop needs to be changed in order to accomodate non integer values of the center
 mask = zeros(size(I));
 mask(bpy,bpx) = 1;
+
+imshow(I);zoom on; title('click on pixels to white out'); pause;
+
+removePix = ginput;
+title('click on the bottom, then rightmost boundary that we allow the BP to travel through');
+boundarys = ginput(2);
+yBound = ceil(boundarys(1,2));
+xBound = ceil(boundarys(2,1));
+
+removePix = round(removePix);
+seRMpix = strel('disk',1);
+m = zeros(size(I));
+if ~isempty(removePix)
+    m(removePix(:,2),removePix(:,1)) = 1;
+end
+
+m = imdilate(m,seRMpix);
+p = regionprops(m,'PixelList');
+xRM = [];
+yRM=[];
+for ii = 1:length(p);
+    tempX = p(ii).PixelList(:,1);
+    xRM = [xRM;tempX];
+    
+    tempY = p(ii).PixelList(:,2);
+    yRM = [yRM;tempY];
+end
+
+
 
 bw = imdilate(mask,se);
 s = regionprops(bw,'extrema');
@@ -43,9 +74,9 @@ region = zeros(size(Isub2));
 region(cc.PixelIdxList{keepRegion}) = 1;
 
 
-c = regionprops(region,'Centroid');
-newBPx = c.Centroid(1);
-newBPy = c.Centroid(2);
+c = regionprops(region,Isub,'WeightedCentroid');
+newBPx = c.WeightedCentroid(1);
+newBPy = c.WeightedCentroid(2);
 
 newBPFull = round([newBPx + xmin,newBPy+ymin]);
 bpOut(firstFrame).x = newBPFull(1);
@@ -56,8 +87,20 @@ for ii = firstFrame+1:lastFrame
     v.seek(ii-1);
     I =v.getframe();
     I =histeq(I);
+    I(yRM,xRM) = 255;
     
-    bp = newBPFull;
+    if newBPFull(1) > xBound | newBPFull(2) >yBound
+        ii =ii-5;
+        v.seek(ii-1);
+        I =v.getframe();
+        I =histeq(I);
+        I(yRM,xRM) = 255;
+        
+        imshow(I);zoom on; pause; title('Basepoint exceeded boundaries, click on the basepoint again');
+        bp = round(ginput(1));
+    else
+        bp = newBPFull;
+    end
     mask = zeros(size(I));
     mask(bp(2),bp(1)) = 1;
     
@@ -88,20 +131,20 @@ for ii = firstFrame+1:lastFrame
     region(cc.PixelIdxList{keepRegion}) = 1;
     
     
-    c = regionprops(region,'Centroid');
-    newBPx = c.Centroid(1);
-    newBPy = c.Centroid(2);
+    c = regionprops(region,Isub,'WeightedCentroid');
+    newBPx = c.WeightedCentroid(1);
+    newBPy = c.WeightedCentroid(2);
     
     newBPFull = round([newBPx + xmin,newBPy+ymin]);
-
+    
     bpOut(ii).x= newBPFull(1);
-    bpOut(ii).y =  newBPFull(2); 
-%     %%% plot sanity checks %%%
-%     imshow(I)
-%     ho
-%     scatter(newBPFull(1),newBPFull(2),'r*')
-%     pause(.01)
-%     cla
+    bpOut(ii).y =  newBPFull(2);
+    %%% plot sanity checks %%%
+    imshow(I)
+    ho
+    scatter(newBPFull(1),newBPFull(2),'r*')
+    pause(.01)
+    cla
 end
 
 
