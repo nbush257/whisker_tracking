4fc90ab4f5b242e6acec305dde22051493e872b7
nbush257 <nbush257@gmail.com>
2015-02-13 15:12:53 -0600

    trackBP now removes any nodes prior to the tracked basepoint and returns an altered wStruct

--- trackBP.m
+++ trackBP.m
@@ -2,7 +2,7 @@
 % functionality that removes certain partsof the frame from becoming
 % possible for the basepoint ( other trimmed whiskers that shouldn't
 % move...)
-function bp = trackBP(vidFileName,wStruct,startFrame,endFrame)
+function wStruct = trackBP(vidFileName,wStruct,startFrame,endFrame)
 
 v = seqIo(vidFileName,'r');
 v.seek(startFrame-1);
@@ -10,19 +10,37 @@ I = v.getframe();
 imshow(I);
 zoom on; title('zoom to the basepoint');pause;
 title('click on the basepoint')
-bp(1,:) = ginput(1)
-for ii = 2:length(wStruct);
-   
-    d = sqrt((wStruct(ii).x-bp(ii-1,1)).^2 + (wStruct(ii).y-bp(ii-1,2)).^2);
-    [minD,bpIdx] = (min(d));
-    if minD>5
-        bp(ii,1) = bp(1,1);
-        bp(ii,2) = bp(1,2);
+bp(1,:) = ginput(1);
+for ii = 1:length(wStruct);
+    if ii == 1
+        d = sqrt((wStruct(ii).x-bp(ii,1)).^2 + (wStruct(ii).y-bp(ii,2)).^2);
+    else
+        
+        
+        d = sqrt((wStruct(ii).x-bp(ii-1,1)).^2 + (wStruct(ii).y-bp(ii-1,2)).^2);
     end
+    [~,bpIdx(ii)] = (min(d));
     
     
-    bp(ii,1) = wStruct(ii).x(bpIdx);
-    bp(ii,2) = wStruct(ii).y(bpIdx);
+    
+    bp(ii,1) = wStruct(ii).x(bpIdx(ii));
+    bp(ii,2) = wStruct(ii).y(bpIdx(ii));
+    if ii~=1
+        bp_movement = sqrt((bp(ii,1)-bp(ii-1,1)).^2 + (bp(ii,2)-bp(ii-1,2)).^2);
+        
+        if bp_movement>5
+            bpIdx(ii) = 1;
+            wStruct(ii).x(1) = bp(ii-1,1);
+            wStruct(ii).y(1) = bp(ii-1,2);
+            bp(ii,1) = bp(ii-1,1);
+            bp(ii,2) = bp(ii-1,2);
+        end
+    end
+end
+for ii =1:length(wStruct)
+    wStruct(ii).x = wStruct(ii).x(bpIdx(ii):end);
+    wStruct(ii).y = wStruct(ii).y(bpIdx(ii):end);
+    wStruct(ii).bpx = bp(ii,1);
+    wStruct(ii).bpy = bp(ii,2);
 end
 
-    
