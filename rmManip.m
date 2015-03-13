2c6779d0a901ad589812f054a890c98013554234
nbush257 <nbush257@gmail.com>
2015-03-13 12:08:33 -0500

    new interpolation for the removing of the manipulator. Using a spline, but now the method can be changed easily if needed.

--- rmManip.m
+++ rmManip.m
@@ -2,45 +2,51 @@ function wStructOut = rmManip(wStruct,manip,startFrame,endFrame);
 
 manip = manip(startFrame:endFrame);
 thresh = 10;
-parfor ii = 1:length(wStruct)
+for ii = 1:length(wStruct)
     
-%     if mod(round(ii/length(wStruct)*100),10)==0
-%         fprintf('. \n')
-%     end
+    %     if mod(round(ii/length(wStruct)*100),10)==0
+    %         fprintf('. \n')
+    %     end
     x = wStruct(ii).x;
     y = wStruct(ii).y;
     time = wStruct(ii).time;
     idx=[];
     for jj = 1:length(x)
-        if isempty(manip{ii})
+        if isempty(manip(ii))
             continue
         end
-        d = sqrt((manip{ii}(1,:)-x(jj)).^2+(manip{ii}(2,:)-y(jj)).^2);
+        d = sqrt((manip(ii).x-x(jj)).^2+(manip(ii).y-y(jj)).^2);
         if any(d<=thresh)
             idx = [idx jj];
         end
-%         plot(d);
-%         pause(.01)
-%         cla
+        %         plot(d);
+        %         pause(.01)
+        %         cla
     end
     %this comment will just remove the points near the manipulator
-%      x(idx) = NaN;
-%      y(idx) = NaN;
-
+%     x(idx) = NaN;
+%     y(idx) = NaN;
+    
     % this section interpolates between the two end points of the removed
     % section
     if length(idx)>1
-        x(idx) = linspace(x(idx(1)),x(idx(end)),length(idx));
-        y(idx) = linspace(y(idx(1)),y(idx(end)),length(idx));
+        newX = linspace(x(idx(1)),x(idx(end)),length(idx));
+        pre5x = x(idx(1)-5:(idx(1)-1));
+        post5x = x(idx(end)+1:(idx(end)+5));
+        pre5y = y(idx(1)-5:(idx(1)-1));
+        post5y = y(idx(end)+1:(idx(end)+5));
+        newY = interp1([pre5x;post5x],[pre5y;post5y],newX,'spline');
+        
+        x(idx) = newX;
+        y(idx) = newY;
     end
     
-        
     
-
+    
+    
     wStructOut(ii).x = double(x);
     wStructOut(ii).y = double(y);
     wStructOut(ii).time = double(time);
 end
 
-    
-    
+
