54858d98d998a026b42fb851f1790a42f596da37
nbush257 <nbush257@gmail.com>
2015-03-30 15:36:34 -0500

    New merge_matching_ts code that is faster and gets all repeats of frames

--- merge_matching_ts.m
+++ merge_matching_ts.m
@@ -1,72 +1,54 @@
-function final_wstruct = merge_matching_ts(wstruct,useX,basepointSmaller)
-%%  function [wstruct] = merge_matching_ts(wstruct)
-%   Inputs
-%       wstruct:            data from .whiskers file
-%       useX:               do we use X-axis to sort the whisker (or y?)
-%       basepointSmaller:   on the given axis, is the basepoint smaller
-%                          than the tip
-%   collapse matching timestamps and add their x-y points together
+function wStructOut = merge_matching_ts(wStruct)
+%% function wStructOut = merge_matching_ts(wStruct)
+% --------------------------------------------------------------------
+% Takes whisk input with multiple tracked whiskers per frame and combines
+% them into a single whisker per frame.
+% --------------------------------------------------------------------
+% INPUTS: 
+%   wStruct = 'whisk' struct
+% OUTPUTS:
+%   wStructOut = 'whisk'-like struct with combined whiskeres from the same
+%   frame. Also casts all values to doubles.
+% --------------------------------------------------------------------
+% Nick Bush 2015_03_30
+% --------------------------------------------------------------------
 
-wtimes = [wstruct.time];
-
-wstruct_norepeats = struct([]);
-thrundreds=300:300:length(wstruct);
-fixed_repeats = nan(length(wstruct),1);
-tenths=round(length(wstruct)/10:length(wstruct)/10:length(wstruct));
-for ii = 1:length(wstruct)
-    x = wstruct(ii).x;
-    y = wstruct(ii).y;
-    this_ent = length(wstruct_norepeats)+1;
-    if sum(wtimes==wstruct(ii).time)>1 && ~ismember(wstruct(ii).time,fixed_repeats)
-        for kk = ii:length(wstruct)
-            if wstruct(ii).time == wstruct(kk).time & ii~=kk
-                x(length(x)+1:length(x)+length(wstruct(kk).x)) = wstruct(kk).x;
-                y(length(y)+1:length(y)+length(wstruct(kk).y)) = wstruct(kk).y;
-                fixed_repeats(ii) = wstruct(ii).time;
-                wtimes(kk) = nan;
-                break
-            end
+% grab times from all frames
+allTimes = [wStruct.time];
+count = 0;
+% iterate over all times between the minimum and maximum of all the times. 
+for ii = min(allTimes):max(allTimes)
+    count = count+1;
+    %% find indices of all frames that share a time with the current time
+    sameTime = find(ismember(allTimes,ii));
+    newX = [];
+    newY = [];
+    
+    %% skip frames with no whiskers tracked. 
+    if isempty(sameTime) 
+        continue
+    end
+    
+    %% Concatenate all x and y coords for same frame times.
+    for jj = 1:length(sameTime)
+        x = wStruct(sameTime(jj)).x;
+        y = wStruct(sameTime(jj)).y;
+        if isrow(x)
+            x = x';
+        end
+        if isrow(y)
+            y = y';
         end
         
+        newX = [newX;x];
+        newY = [newY;y];
+    end % End single frame for loop
+    %% Outputs
+    wStructOut(count).x = double(newX);
+    wStructOut(count).y = double(newY);
+    wStructOut(count).time = double(ii);
+end% End all time frames for loop
+end% EOF
+
+
         
-        [x,y] = sortWhisker(x,y,useX,basepointSmaller);
-        wstruct_norepeats(this_ent).x = x;
-        wstruct_norepeats(this_ent).y = y;
-        wstruct_norepeats(this_ent).time = wstruct(ii).time;
-        
-    elseif ~ismember(wstruct(ii).time,fixed_repeats)
-        [x,y] = sortWhisker(x,y,useX,basepointSmaller);
-        wstruct_norepeats(this_ent).x = x;
-        wstruct_norepeats(this_ent).y = y;
-        wstruct_norepeats(this_ent).time = wstruct(ii).time;
-    end
-    
-    if ismember(ii,thrundreds)
-        fprintf('.')
-    elseif ismember(ii,tenths)
-        fprintf(['\n',num2str(100*ii/length(wstruct)),' percent complete\n'])
-    end
-    
-end
-
-
-%%   Order struct chronologically
-clear wtimes
-fprintf('\n\t Ordering chronologically')
-for ii = 1:length(wstruct_norepeats)
-    wtimes(ii) = wstruct(ii).time;
-end
-
-[corr_order,indicies] = sort(wtimes);
-thrundreds=300:300:length(wstruct_norepeats);
-tenths=round(length(wstruct_norepeats)/10:length(wstruct_norepeats)/10:length(wstruct_norepeats));
-for ii = 1:length(wstruct_norepeats);
-    final_wstruct(ii) = wstruct_norepeats(indicies(ii));
-     if ismember(ii,thrundreds)
-        fprintf('.')
-    elseif ismember(ii,tenths)
-        fprintf(['\n',num2str(100*ii/length(wstruct)),' percent complete\n'])
-    end
-end
-
-fprintf(['\n\n\tRemoved/combined ',num2str(length(wstruct)-length(wstruct_norepeats)),' repeated frames\n\n'])
