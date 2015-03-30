72da43a4f67ee656da70e45421286fa92180e963
nbush257 <nbush257@gmail.com>
2015-03-30 17:27:53 -0500

    created check data existence, which makes sure your path/filenames point to real files before you run the merging code

---
+++ checkDataExistence.m
@@ -0,0 +1,30 @@
+if ~exist(frontWhiskersName,'file')
+    warning('Front .whisker Filename incorrect')
+end
+
+if ~exist(topWhiskersName,'file')
+    warning('Top .whisker Filename incorrect')
+end
+
+if ~exist(frontVidName,'file')
+    warning('Front Video Filename incorrect')
+end
+
+if ~exist(topVidName,'file')
+    warning('Top Video Filename incorrect')
+end
+
+if ~exist(frontManipulatorName,'file')
+    warning('Front Manipulator Filename incorrect')
+end
+if ~exist(topManipulatorName,'file')
+    warning('Top Manipulator Filename incorrect')
+end
+
+if ~exist(contactName,'file')
+    warning('Contact Filename incorrect')
+end
+
+if ~exist(calibName,'file')
+    warning('Calibration Filename incorrect')
+end
