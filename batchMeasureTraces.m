532a409250a750979a9982845b3a3a8d6815709d
nbush257 <nbush257@gmail.com>
2015-07-28 17:19:32 -0500

    parallelized batchmeasureTraces

--- batchMeasureTraces.m
+++ batchMeasureTraces.m
@@ -1,14 +1,29 @@
 d = dir('*.whiskers');
-bp(1) = 550;
-bp(2) = 400;
-fol = 350;
+bp(1) = 90;
+bp(2) = 332;
+fol =122;
+direction = 'v';
 for ii = 1:length(d)
-    
+    copyfile(d(ii).name,[d(ii).name(1:end-9) '_whisker.whiskers'])
+    if ii == 1
+        
+        measurementsFileName = [d(ii).name(1:end-9) '_whisker.measurements'];
+        measureStr = sprintf('measure --face %i %i %s %s %s',bp(1),bp(2),direction,d(ii).name,measurementsFileName)
+        system(measureStr)
+        
+        classifyStr = sprintf('classify %s %s %i %i %s --px2mm .04 --follicle %i -n 1',measurementsFileName,measurementsFileName,bp(1),bp(2),direction,fol)
+        reclassifyStr = sprintf('reclassify %s %s -n 1',measurementsFileName,measurementsFileName);
+        
+        system(classifyStr)
+        system(reclassifyStr)
+    end
+end
+parfor ii = 2:length(d)
     measurementsFileName = [d(ii).name(1:end-9) '_whisker.measurements'];
-    measureStr = sprintf('measure --face %i %i h %s %s',bp(1),bp(2),d(ii).name,measurementsFileName)
+    measureStr = sprintf('measure --face %i %i %s %s %s',bp(1),bp(2),direction,d(ii).name,measurementsFileName)
     system(measureStr)
     
-    classifyStr = sprintf('classify %s %s %i %i h --px2mm .04 --follicle %i -n 1',measurementsFileName,measurementsFileName,bp(1),bp(2),fol)
+    classifyStr = sprintf('classify %s %s %i %i %s --px2mm .04 --follicle %i -n 1',measurementsFileName,measurementsFileName,bp(1),bp(2),direction,fol)
     reclassifyStr = sprintf('reclassify %s %s -n 1',measurementsFileName,measurementsFileName);
     
     system(classifyStr)
