fe73376a3b2907a7b637ee4a386cad4f3c7979bc
nbush257 <nbush257@gmail.com>
2015-12-18 15:27:44 -0600

    Just started 3dBP cleaning function

---
+++ clean3D_BP.m
@@ -0,0 +1,5 @@
+function wStruct3D_out = clean3D_BP(wstruct3D)
+%% function wStruct3D_out = clean3D_BP(wstruct3D)
+% Once you have a merged 3D whisker we should smooth the basepoint again in
+% 3D
+
