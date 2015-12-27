01c1e3526a4958078192e9b6f0c99385fcbaf982
nbush257 <nbush257@gmail.com>
2015-12-27 17:50:26 -0600

    created a function to apply robust loess smoothing to the 2D whisker

---
+++ smooth2D_whisker.m
@@ -0,0 +1,14 @@
+function smoothed = smooth2D_whisker(wStruct)
+%% function smoothed = smooth2D_whisker(wStruct)
+% Apply robust loess smoothing to each 2D whisker.
+smoothed = wStruct;
+h = waitbar(0,'Smoothing 2D whisker');
+for ii = 1:length(wStruct)
+    waitbar(ii/length(wStruct),h,'Smoothing 2D whisker')
+    if isempty(wStruct(ii).x)
+        continue
+    end
+    smoothed(ii).x = wStruct(ii).x;
+    smoothed(ii).y = smooth(wStruct(ii).x,wStruct(ii).y,.1,'rloess');
+end
+delete(h)
