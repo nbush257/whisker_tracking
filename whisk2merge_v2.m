d736c9822eb4c17b6619c19a9043fc36676e2e91
nbush257 <nbush257@gmail.com>
2015-12-28 11:13:52 -0600

    started new contact algorithm using curvature

--- whisk2merge_v2.m
+++ whisk2merge_v2.m
@@ -50,3 +50,50 @@ for ii = 1:800
     plot(tw(sample(ii)).x(1),tw(sample(ii)).y(1),'r*')
 end
 %% Get contact 
+% get theta for contact estimation
+TH_top = nan(length(tw),1);
+for ii = 1:length(tw)
+    if isempty(tw(ii).x)
+        continue
+    end
+    x1 = tw(ii).x(1);
+    y1 = tw(ii).y(1);
+    l = length(tw(ii).x);
+    ye = tw(ii).y(ceil(l/5));
+    xe = tw(ii).x(ceil(l/5));
+    TH_top(ii) = atan2(ye-y1,xe-x1)*180/pi;
+end
+
+% wrap theta
+TH_top(TH_top>nanmean(TH_top)+180) = TH_top(TH_top>nanmean(TH_top)+180)-360;
+TH_top(TH_top<nanmean(TH_top)-180) = TH_top(TH_top<nanmean(TH_top)-180)+360;
+TH_top = double(TH_top);
+
+% get theta front
+TH_front = nan(length(fw,1));
+for ii = 1:length(fw)
+    if isempty(fw(ii).x)
+        continue
+    end
+    x1 = fw(ii).x(1);
+    y1 = fw(ii).y(1);
+    l = length(fw(ii).x);
+    ye = fw(ii).y(ceil(l/5));
+    xe = fw(ii).x(ceil(l/5));
+    TH_front(ii) = atan2(ye-y1,xe-x1)*180/pi;
+end
+
+% wrap theta
+TH_front(TH_front>nanmean(TH_front)+180) = TH_front(TH_front>nanmean(TH_front)+180)-360;
+TH_front(TH_front<nanmean(TH_front)-180) = TH_front(TH_front<nanmean(TH_front)-180)+360;
+TH_front = double(TH_front);
+
+
+
+
+k(:,1) = medfilt1([twM.curvature],5);
+k(:,2) = medfilt1([fwM.curvature],5);
+k(:,3) = medfilt1(TH_top);
+k(:,4) = medfilt1(TH_front);
+
+
