ffa7ae10983da448efef44d19bf762946b5cb82a
nbush257 <nbush257@gmail.com>
2016-04-27 13:09:57 -0500

    comments and clean up of smooth3d code. No major changes to algorithm

--- smooth3DWhisker.m
+++ smooth3DWhisker.m
@@ -1,43 +1,46 @@
 function wOut = smooth3DWhisker(wIn)
 %% function wStruct_3DOut = smooth3DWhisker(wStruct_3D)
-% takes a 3D whisker structure and smooths itusing loess smoothing. Should
+% ========================================
+% takes a 3D whisker structure and smooths it using lowess smoothing. Should
 % smooth out the basepoint and kinks.
-% NB 2016_01_25
+% =======================================
+% INPUTS:
+%           wIn - a 3D whisker structure.
+% OUTPUTS:
+%           wOut - a 3D whisker structure that has been smoothed
+% =======================================
+% NB 2016_04_27
 % Issue with row or column vectors. need to rewrite some other code to get
-% the 3d struct back asa  column.
-warning('This function has not been properly tested. Sanity checks are strongly encouraged')
+% the 3d struct back as a  column.
 wOut = wIn;
-
+fprintf('Smoothing')
 parfor ii = 1:length(wIn)
     % skip empty entries
-    if isempty(wIn(ii).x)|isempty(wIn(ii).y)|isempty(wIn(ii).z)
+    if isempty(wIn(ii).x)|| isempty(wIn(ii).y) || isempty(wIn(ii).z)
         continue
     end
     % skip whiskers that are too short
     if length(wIn(ii).x)<10
         continue
     end
-    if isrow(wIn(ii).x)
-        try
-            wOut(ii).y = smooth(wIn(ii).x,wIn(ii).y,'rlowess',.3);
-            wOut(ii).z = smooth(wIn(ii).x,wIn(ii).z,'rlowess',.3);
-%             f = fit([wIn(ii).x' wOut(ii).y],wIn(ii).z','lowess','Robust','LAR');
-%             wOut(ii).z = feval(f,wIn(ii).x,wIn(ii).y)';
-        catch
-            disp('error')
-        end
-    else
-        % removing the Robust option may speed it up, but may also be
-        % ssusceptible to basepoint errors.
-        try
-            f = fit([wIn(ii).x wIn(ii).y],wIn(ii).z,'poly22');
-            wOut(ii).z = feval(f,wIn(ii).x,wIn(ii).y);
-        end
+    
+    % smooth the whisker
+    try
+        wOut(ii).y = smooth(wIn(ii).x,wIn(ii).y,'rlowess',.3);
+        wOut(ii).z = smooth(wIn(ii).x,wIn(ii).z,'rlowess',.3);
+    catch
+        disp('error')
     end
     
-    if mod(ii,1000) ==0
-        fprintf('Frame\t%i\n',ii)
+    %% verbosity
+    if mod(ii,round(length(wIn)/100)) == 0
+        fprintf('.')
+    end
+    if mod(ii,round(length(wIn)/10)) ==0
+        fprintf('\n')
     end
     
+end % end parfor over frames
+
 end
 
