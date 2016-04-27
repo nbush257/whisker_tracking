97716eecd12203775a3a6ed3291dfa0d6ca330ad
nbush257 <nbush257@gmail.com>
2016-04-27 07:18:45 -0500

    smooth3D now uses poly22

--- get3DCP_hough.m
+++ get3DCP_hough.m
@@ -1,4 +1,4 @@
-function CP = get3DCP_hough(Y0_f,Y1_f,Y0_t,Y1_t,tracked3D,calibInfo,C)
+function [CP,tracked3D] = get3DCP_hough(Y0_f,Y1_f,Y0_t,Y1_t,tracked3D,calibInfo,C)
 %% function CP = get3DCP_hough(Y0,Y1,tracked3D,calibInfo)
 % Calculates the 3D contact point by backprojecting the tracked 3D whisker
 % into 2D and finding the intersection.
@@ -16,12 +16,13 @@ function CP = get3DCP_hough(Y0_f,Y1_f,Y0_t,Y1_t,tracked3D,calibInfo,C)
 %           CP = [numFrames x 3] matrix of the contact point in tracked 3D
 %           space. Units are in the same units as tracked3D(should be mm)
 % =========================================================================
-
 CP = nan(length(tracked3D),3);
 CPidx = nan(length(tracked3D),1);
 l_thresh = 10; % fewest number of points allowed in the whisker for CP calculation
 missingMan = logical(zeros(length(C),1));
-for ii = 1:length(tracked3D)
+parfor ii = 1:length(tracked3D)
+    warning('off')
+
     if ~C(ii)
         continue
     end
@@ -33,7 +34,7 @@ for ii = 1:length(tracked3D)
         px = [0;640];
         py = [Y0_t(ii);Y1_t(ii)];
         [~,wskrTop] = BackProject3D(tracked3D(ii),calibInfo(5:8),calibInfo(1:4),calibInfo(9:10));
-        [x1,y1,idx,~] = intersections(wskrTop(:,1),wskrTop(:,2),px,py);        
+        [x1,y1,idx,~] = intersections(wskrTop(:,1),wskrTop(:,2),px,py);
         
     elseif ~isnan(Y0_f(ii))
         px = [0;640];
@@ -42,6 +43,7 @@ for ii = 1:length(tracked3D)
         [x1,y1,idx,~] = intersections(wskrFront(:,1),wskrFront(:,2),px,py);
     else
         missingMan(ii) = 1;
+        continue
     end
     
     
@@ -49,28 +51,61 @@ for ii = 1:length(tracked3D)
     %% EVERYTHING BELOW HERE IS NOT FINISHED
     
     counter =0;
-        while isempty(idx) | idx>=(length(tracked3D(ii).x)+10)
-            counter = counter+1;
-            
-            xyfit = polyfit(tracked3D(ii).x,tracked3D(ii).y,3);
-            xzfit = polyfit(tracked3D(ii).x,tracked3D(ii).z,3);
-            [CPx,CPy,tempCPidx,tempTracked] = LOCAL_extend_one_Seg(tracked3D(ii),xyfit,xzfit,px,py,calibInfo(5:8),calibInfo(1:4),calibInfo(9:10),1);
-            tracked3D(ii) = tempTracked;
-            
-            if counter>100
-                        [wskrTop,wskrFront] = BackProject3D(tracked3D(ii),calib(5:8),calibInfo(1:4),calibInfo(9:10));  
-                        figure(123)
-                plot(wskrFront(:,1),wskrFront(:,2),'.')
-                ho
-                plot(px,py,'o')
-                break
-            end
-                
-        end
+    while isempty(idx) | (idx+10)>=(length(tracked3D(ii).x))
+        counter = counter+1;
         
+        xyfit = polyfit(tracked3D(ii).x,tracked3D(ii).y,3);
+        xzfit = polyfit(tracked3D(ii).x,tracked3D(ii).z,3);
+        
+        [CPx,CPy,idx,tempTracked] = LOCAL_extend_one_Seg(tracked3D(ii),xyfit,xzfit,px,py,calibInfo(5:8),calibInfo(1:4),calibInfo(9:10),~isnan(Y0_t(ii)));
+        
+        if counter>100
+            disp('serious problem')
+            %                         [wskrTop,wskrFront] = BackProject3D(tracked3D(ii),calibInfo(5:8),calibInfo(1:4),calibInfo(9:10));
+            %                         figure(123)
+            %                 plot(wskrFront(:,1),wskrFront(:,2),'.')
+            %                 ho
+            %                 plot(px,py,'o')
+            break
+        end
+        % plot is always turned off during normal code running. These lines
+        % are here to remind you what to plot
+        plotTGL = 0;
+        if plotTGL
+            close all
+            [wskrTop,wskrFront] = BackProject3D(tracked3D(ii),calibInfo(5:8),calibInfo(1:4),calibInfo(9:10));
+            [wskrTopext,wskrFrontext] = BackProject3D(tempTracked,calibInfo(5:8),calibInfo(1:4),calibInfo(9:10));
+            plot(px,py,'r')
+            ho
+            if ~isnan(Y0_t(ii))
+                plotv(wskrTop,'.')
+                plotv(wskrTopext,'go')
+                
+            else
+                plotv(wskrFront,'.')
+                plotv(wskrFrontext,'go')
+            end
+            figure
+            plot3(tracked3D(ii).x,tracked3D(ii).y,tracked3D(ii).z,'.')
+            ho
+            plot3(tempTracked.x,tempTracked.y,tempTracked.z,'go')
+            pause
+        end
+        tracked3D(ii) = tempTracked;
+
+    end
+    CPidx(ii) = idx(1);
+    ridx = round(idx(1));
+    CP(ii,:) = [tracked3D(ii).x(ridx) tracked3D(ii).y(ridx) tracked3D(ii).z(ridx)]; ;
+    
+    %% Update on status
+    if mod(ii,1000) == 0
+        fprintf('Frame: \t%i\n',ii)
+    end
     
     
 end
+warning('on')
 end
 
 function [CPx,CPy,tempCPidx,wskr3D] = LOCAL_extend_one_Seg(wskr3D,whfitA,whfitB,px,py,A_camera,B_camera,A2B_transform,useFront)
@@ -82,7 +117,7 @@ end
 
 [CPx,CPy,tempCPidx,~] = intersections(wskr(:,1),wskr(:,2),px,py);
 
-if tempCPidx+10<length(wskr(:,1))
+if (tempCPidx+10)<length(wskr(:,1))
     return
     
 else
--- smooth3DWhisker.m
+++ smooth3DWhisker.m
@@ -1,9 +1,10 @@
 function wOut = smooth3DWhisker(wIn)
 %% function wStruct_3DOut = smooth3DWhisker(wStruct_3D)
 % takes a 3D whisker structure and smooths itusing loess smoothing. Should
-% smooth out the basepoint and kinks. 
+% smooth out the basepoint and kinks.
 % NB 2016_01_25
-
+% Issue with row or column vectors. need to rewrite some other code to get
+% the 3d struct back asa  column.
 warning('This function has not been properly tested. Sanity checks are strongly encouraged')
 wOut = wIn;
 
@@ -16,10 +17,25 @@ parfor ii = 1:length(wIn)
     if length(wIn(ii).x)<10
         continue
     end
+    if isrow(wIn(ii).x)
+        try
+            f = fit([wIn(ii).x' wIn(ii).y'],wIn(ii).z','poly22');
+            wOut(ii).z = feval(f,wIn(ii).x,wIn(ii).y)';
+        catch
+            disp('error')
+        end
+    else
+        % removing the Robust option may speed it up, but may also be
+        % ssusceptible to basepoint errors.
+        try
+            f = fit([wIn(ii).x wIn(ii).y],wIn(ii).z,'poly22');
+            wOut(ii).z = feval(f,wIn(ii).x,wIn(ii).y);
+        end
+    end
     
-    % removing the Robust option may speed it up, but may also be
-    % ssusceptible to basepoint errors.
-    f = fit([wIn(ii).x wIn(ii).y],wIn(ii).z,'loess','Robust','LAR');
-    wOut(ii).z = feval(f,wIn(ii).x,wIn(ii).y);
+    if mod(ii,1000) ==0
+        fprintf('Frame\t%i\n',ii)
+    end
+    
 end
 
