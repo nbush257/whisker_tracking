ff75353889460627090663290549c7bfc8cf45f8
nbush257 <nbush257@gmail.com>
2016-05-04 12:06:43 -0500

    added simple code to sort whisker based on ascending x coordinate

--- sort3Dwhisker.m
+++ sort3Dwhisker.m
@@ -1,10 +1,19 @@
 function w = sort3Dwhisker(w)
-
+% Sorts the whisker based on the x value, and makes all vectors column
+% vectors.
 parfor ii = 1:length(w)
     if isempty(w(ii).x)
         continue
     end
-    [w(ii).x,idx] = sort(w(ii).x);
-    w(ii).y = w(ii).y(idx);
-    w(ii).z = w(ii).z(idx);
+    if isrow(w(ii).x)
+        [w(ii).x,idx] = sort(w(ii).x');
+        w(ii).y = w(ii).y(idx)';
+        w(ii).z = w(ii).z(idx)';
+    else
+        
+        [w(ii).x,idx] = sort(w(ii).x);
+        w(ii).y = w(ii).y(idx);
+        w(ii).z = w(ii).z(idx);
+    end
+    
 end
