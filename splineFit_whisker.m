0265c693dd72d3e5cd1326cb7c28c536580ca37b
nbush257 <nbush257@gmail.com>
2016-05-11 14:14:31 -0500

    In splineFit_whisker: plotting is now toggleable from an optional argument

--- splineFit_whisker.m
+++ splineFit_whisker.m
@@ -1,9 +1,9 @@
-function [xs,ys] = splineFit_whisker(xw,yw,numNodes)
-%% function xs,ys = splineFit_whisker(xw,yw,[numNodes])
+function [xs,ys] = splineFit_whisker(xw,yw,numNodes,plotTGL)
+%% function xs,ys = splineFit_whisker(xw,yw,[numNodes],[plotTGL])
 % Smooths a 2D whisker by using splinefit from Jonas Lundgren
 % =================================================
 % INPUTS:   xw - a N cell array of whisker x points (each cell is a frame)
-%           yw - a N element cell array of whisker y points 
+%           yw - a N element cell array of whisker y points
 %           numNodes - number of nodes to use in the splinefit. Chris seemed
 %           to use 4 most of the time.
 % OUTPUTS:  xs - smoothed x points
@@ -15,7 +15,13 @@ function [xs,ys] = splineFit_whisker(xw,yw,numNodes)
 
 if nargin <=2
     numNodes = 4;
+    plotTGL = 0;
 end
+if nargin <=3
+    plotTGL = 0;
+end
+
+
 
 %% Fit spline to data
 xs = [];
@@ -28,14 +34,15 @@ for ii = 1:length(xw),
         PP = splinefit(xw{ii},yw{ii},numNodes);
         xx = min(xw{ii}):1:max(xw{ii});
         yy = ppval(PP,xx);
-        xs{ii} = xx; 
+        xs{ii} = xx;
         ys{ii} = yy;
-        if TGL_plotsteps,
+        if mod(ii,100)==0 && plotTGL
             figure(3000); clf(3000);
             plot(xw{ii},yw{ii},'b.'); hold on;
             plot(xx,yy,'r-');
             title(['Step: ',num2str(ii)],'FontWeight','bold');
             axis equal;
+            drawnow
         end
     else
         xs{ii} = [];
