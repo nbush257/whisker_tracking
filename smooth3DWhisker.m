abad56b38f4cc714228aabd99b680c61a05b806b
nbush257 <nbush257@gmail.com>
2016-05-12 10:28:52 -0500

    smooth3DWhisker is better commented, defaults to splinefit, and accepts optional arguments for splinefit and extending.

--- smooth3DWhisker.m
+++ smooth3DWhisker.m
@@ -1,22 +1,46 @@
-function wOut = smooth3DWhisker(wIn,method)
-%% function wStruct_3DOut = smooth3DWhisker(wStruct_3D,[method])
+function wOut = smooth3DWhisker(wIn,mode,numNodes,extend)
+%% function wStruct_3DOut = smooth3DWhisker(wStruct_3D,[method],[numNodes],[extend])
 % ========================================
-% takes a 3D whisker structure and smooths it using lowess smoothing. Should
-% smooth out the basepoint and kinks. MAYBE WE WANT TO USE SPLINEFIT!!!
+% takes a 3D whisker structure and smooths it. Should
+% smooth out the basepoint and kinks. Can operate in either splinefit mode
+% or lowess mode. 
+% 
+% Splinefit mode offers advantages of being faster, and being able to extend
+% and interpolate the whisker. However, it assumes that x points are 
+% sorted as ascending in the x coordinate. If this is not true, then it 
+% will extend the basepoint. 
+%
+% Lowess is slower, but also doesn't compensate the whisker shape as easily
+% as splinefit. 
+% 
 % =======================================
 % INPUTS:
 %           wIn - a 3D whisker structure.
+%           [mode] - a string either 'spline' or 'linear'
+%           [numNodes] - optional number for spline input. Gets ignored if
+%               mode is linear. Default is 4
+%           [extend] - Tells splinefit how much to extend the whisker by
+%               (as a percentage of the x coordinate extent). e.g. .1 extends
+%               the whisker by 10%. Default is 0.1
 % OUTPUTS:
 %           wOut - a 3D whisker structure that has been smoothed
 % =======================================
 % NB 2016_04_27
 % Issue with row or column vectors. need to rewrite some other code to get
 % the 3d struct back as a  column.
-%% 
-if nargin==1
-    method = 'spline';
+%% Input handling
+switch nargin
+    case 1
+        mode = 'spline';
+        numNodes = 4;
+        extend = 0.1;
+    case 2
+        numNodes = 4;
+        extend = 0.1;
+    case 3
+        extend = 0.1;
 end
-numNodes = 4;
+
 wOut = wIn;
 fprintf('Smoothing')
 parfor ii = 1:length(wIn)
@@ -30,10 +54,10 @@ parfor ii = 1:length(wIn)
     end
     
     % smooth the whisker
-    switch method
+    switch mode
         
         % implement robust lowess smoothing
-        case 'loess'
+        case 'linear'
             try
                 wOut(ii).y = smooth(wIn(ii).x,wIn(ii).y,'rlowess',.3);
                 wOut(ii).z = smooth(wIn(ii).x,wIn(ii).z,'rlowess',.3);
@@ -44,7 +68,7 @@ parfor ii = 1:length(wIn)
             
         case 'spline'
             PP = splinefit(wIn(ii).x,wIn(ii).y,numNodes,'r');
-            xx = min(wIn(ii).x):.1:(max(wIn(ii).x));
+            xx = min(wIn(ii).x):.1:(max(wIn(ii).x))+abs(extend*max(wIn(ii).x));
             yy = ppval(PP,xx);
             wOut(ii).x = xx; 
             wOut(ii).y = yy;
