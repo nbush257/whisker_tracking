c8b58c40058907b89e0c890f43f243d81dfce639
nbush257 <nbush257@gmail.com>
2016-06-08 15:38:18 -0500

    edited merge2E3D to account for 3D contact determining. also suggests saves incrementally

--- merge2E3D_v3.m
+++ merge2E3D_v3.m
@@ -1,52 +1,53 @@
-function 
+%% init saving filename
+fname = ''
+
 % start parallel pool
 parpool
 % sort the whisker along the x axis
 t3d = sort3Dwhisker(tracked_3D);
+
 % smooth the whisker
-t3ds = smooth3DWhisker(t3d);
-
+t3d= smooth3DWhisker(t3d);
+save(fname)
+% get contact manually
+C = getContact_from3D(t3d);
+save(fname)
 % Find the contact point and extend whisker where needed
-
-[CP,~,t3dsext] = get3DCP_hough(manip,t3ds,calib,C);
-
-% smooth the basepoint
-
-
+[CPraw,~,t3d] = get3DCP_hough(manip,t3d,calib,C);
+save(fname)
 % smooth the contact point
-CPout = cleanCP(CP);
+CP = cleanCP(CPraw);
 
 % In case the contact point is not on the whisker after smoothing, put it
 % back on the whisker.
 
-[~,CPclean] = CPonWhisker(CPout,t3dsext);
+[~,CP] = CPonWhisker(CP,t3d);
 
 % Prepare for E3D
-xw3d = {t3dsext.x};
-yw3d = {t3dsext.y};
-zw3d = {t3dsext.z};
+xw3d = {t3d.x};
+yw3d = {t3d.y};
+zw3d = {t3d.z};
 
-% clean first few nodes
-BP = get3DBP(t3dsext);
+% extract the basepoint
+BP = get3DBP(t3d);
+save(fname)
 
 %% data QC
 figure
-for ii = find(C,1):1000:length(t3dsext)
+for ii = find(C,1):1000:length(t3d)
     ho
     cla
     plot3(tracked_3D(ii).x,tracked_3D(ii).y,tracked_3D(ii).z,'k.-')
     
-    plot3(t3dsext(ii).x,t3dsext(ii).y,t3dsext(ii).z,'.','color',[0.5 0.3 0.7])
+    plot3(t3d(ii).x,t3d(ii).y,t3d(ii).z,'.','color',[0.5 0.3 0.7])
     
-    plot3(CPclean(ii,1),CPclean(ii,2),CPclean(ii,3),'r*')
+    plot3(CP(ii,1),CP(ii,2),CP(ii,3),'r*')
     
     plot3(BP(ii,1),BP(ii,2),BP(ii,3),'b^')
     
     drawnow
     pause(.05)
     
-    
-    
-    
 end
+
 
