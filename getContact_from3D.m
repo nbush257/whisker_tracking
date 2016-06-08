9b1c23efa4d035bcc4522bef1b424ff794886807
nbush257 <nbush257@gmail.com>
2016-06-08 15:32:56 -0500

    cleaned up getContactfrom3D. It is now a function as well.

--- getContact_from3D.m
+++ getContact_from3D.m
@@ -1,86 +1,105 @@
-X = featureScaling(PP);
-[a,b,c] = pca(X);
 %%
-b1 = b(:,1);
-b1(isnan(b1)) = nanmean(b1);
-b1 = bwfilt(b1,300,0,10);
-plot(diff(b1))
-%%
-for ii = 1:size(X,2)
-    subplot(6,6,ii)
-    plot(X(:,ii))
-end
+function C = getContact_from3D(w,varargin)
+%% function C = getContact_from3D(w,[C],[winsize])
+% takes a 3D whisker structure as input and asks the user to manually find
+% contact periods based on a PCA decomposition of the tip location.
+% INPUTS:   w - a 3D whisker struct
+
+%           [C] - a [length(w) by 1] contact logical. This is used if you have found part of
+%           the C variable, but not all of it. If a C variable is input,
+%           the user inputting starts at the last True value of C.
+%       
+%           [winsize] - how much of the tip position to view at a time.
+
+% OUTPUTS:  C - a [length(w) x 1] contact logical 
+% NEB 2016_06_08
+%% varargin handling
+narginchk(1,3)
+numvargs = length(varargin);
+optargs = {false(length(w)),5000;};
+optargs(1:numvargs) = varargin;
+[C,winsize] = optargs{:};
 
 %%
-tip = nan(length(w),3);
+close all
 
-for ii = 1:length(w)
-    if isempty(w(ii).x)
-        continue
-    end
-    
-    tip(ii,:) = [w(ii).x(end) w(ii).y(end) w(ii).z(end)];
-end
-%%
+%get a smoothed estimate of tip position
 tip_clean = clean3D_tip(w);
-%%
+
+% decompose the tip location into a 1D vector.
 [~,b] = pca(featureScaling(tip_clean));
 bb = b(:,1);
 plot(bb)
+
+% this might need fixing. It is a workaround to make everythin positive
 [~,y] = ginput(1);
 bb = abs(b(:,1)-y);
-%%
-% [p,l,w] = findpeaks(bb,'minpeakprominence',nanstd(bb)/3,'minpeakwidth',4);
+%% Manual input
+close all
 
-% 
-% cStart = round(l-w);cEnd = round(l+w);
-% cStart(cStart<1) = 1;
-% cEnd(cEnd>length(C)) = length(C);
-C = false(size(bb));
-% for ii = 1:length(cStart)
-%     C(cStart(ii):cEnd(ii)) = 1;
-% end
-
-
+% init windowing
 starts = 1;
-winsize = 5000;
+if numvargs >= 1 && sum(C)>0
+    starts = find(C,1,'last');
+end
+
 stops = winsize+starts;
 longfig
+
+% set window in which to look for the minimum value of abs(bb). This makes
+% it easier to click quickly.
 slop = 10;
-while starts<length(C)
-    x = 0;
-    if stops>length(C)
-        stops = length(C);
-    end
-    while ~isempty(x)
-        clf
-        plot(scale(bb(starts:stops)),'k');ln2;
-        shadeVector(C(starts:stops))
+%try statement so that you don't lose all your work if something stupid
+%happens
+try
+    while starts<length(C)
+        % x is the ginput x positions. Init to allow for a while loop in a
+        % few lines
+        x = 0;
         
-        [x,~,but] = ginput(2);
-        x = sort(x);
-        x(x<1)=1;
-        x = round(x);
-        x = x+starts;
-        if isempty(x)
-            continue
+        % prevent indexing past the length of the trace
+        if stops>length(C)
+            stops = length(C);
         end
         
-        [~,t1] = min(bb(x(1)-slop:x(1)+slop));
-        
-        
-        xy(1) = x(1)-slop-1+t1;
-        xy(2) = x(2)
-        
-        if but ==1
-            C(xy(1):xy(2)) = 1;
-        elseif but==3
-            C(xy(1):xy(2)) = 0;
+        % stay on this window until no inputs.
+        while ~isempty(x)
+            clf
+            plot(scale(bb(starts:stops)),'k');ln2;
+            shadeVector(C(starts:stops))
+            
+            % get user inputs
+            [x,~,but] = ginput(2);
+            x = sort(x);
+            x(x<1)=1;
+            x = round(x);
+            x = x+starts;
+            if isempty(x)
+                continue
+            end
+            
+            % find the minimum within a window the size of slop for the contact onset. May want
+            % to switch to a local min somehow.
+            [~,t1] = min(bb(x(1)-slop:x(1)+slop));
+            xy(1) = x(1)-slop-1+t1;
+            xy(2) = x(2);
+            
+            % If left click, add the region to contact. If right click,
+            % remove the region from contact
+            if but ==1
+                C(xy(1):xy(2)) = 1;
+            elseif but==3
+                C(xy(1):xy(2)) = 0;
+            end
         end
+        
+        hold off
+        % get the next window
+        starts = stops;
+        stops = starts+winsize;
     end
-    hold off
-    starts = stops;
-    stops = starts+winsize;
+catch
+    fprintf(repmat('=',20,1))
+    fprintf('\nFunction errored out. Returning C variable\n')
+    return
 end
-ca
-plot(C)
