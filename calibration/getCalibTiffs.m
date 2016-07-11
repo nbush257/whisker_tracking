d0e22246fe0509446adce3f0d6bd1840c3281bd0
nbush257 <nbush257@gmail.com>
2016-07-11 10:01:08 -0500

    Started editing getCalibTiffs. This function is likey deprecated and replaced by 'autoExtractCorners' and 'fullAutoCalib'.

--- calibration/getCalibTiffs.m
+++ calibration/getCalibTiffs.m
@@ -1,8 +1,40 @@
+function getCalibTiffs(trialString)
 % get calibration tiffs
 % use this script to write a set of caliration images to tiffs.
-cd C:\Users\guru\Documents\hartmann_lab\data\2016_02
-topVidName = 'rat2016_02_JAN27_VG_E3_t01_Top_calib.seq'
-frontVidName = 'rat2016_02_JAN27_VG_E3_t01_Front_calib.seq'
+%% Get the file name.
+
+% Try to match the front and top calib files with the input argument
+topVidName = [trialString 'Top_calib.seq'];
+frontVidName = [trialString 'Front_calib.seq'];
+
+% if no arguments or the file names dont exist
+if nargin>0 || ~exist(topVidName,'file') || ~exist(frontVidName,'file')
+    warning('Files in argument not found. Opening filefinder...')
+    try
+        [templateStr,pName] = uigetfile('*.seq','Choose one of the videos for this calibration.The corresponding alternate view will be automatically chosen');
+        cd(pName)
+        d = dir([templateStr(1:regexp(templateStr,'t\d\d_','end')) '*calib*.seq']);
+        
+        assert(length(d)==2)
+        
+        % Find the appropriate view from the filename
+        if regexp(d(1).name,'Front')
+            frontVidName = d(1).name;
+            topVidName = d(2).name;
+        else
+            frontVidName = d(2).name;
+            topVidName = d(1).name;
+        end
+        
+    catch
+        % If there is an error, open two dialog boxes to get the files
+        [frontVidName,pName] = uigetFile('*.seq','Choose the FRONT Calibration Video')
+        cd(pName)
+        [topVidName] = uigetFile('*.seq','Choose the TOP Calibration Video')
+    end
+end
+
+
 frames2grab = frames;
 top = seqIo(topVidName,'r');
 front = seqIo(frontVidName,'r');
@@ -18,9 +50,9 @@ for i = frames2grab
     top.seek(i-1);
     front.seek(i-2);
     fI = front.getframe();
-%     fI = adapthisteq(fI);
+    %     fI = adapthisteq(fI);
     tI = top.getframe();
-%     tI = adapthisteq(tI);
+    %     tI = adapthisteq(tI);
     cd(topTifDir)
     imwrite(tI,['top' int2str(count) '.tif'],'tif')
     cd ..
@@ -29,4 +61,3 @@ for i = frames2grab
     cd ..
 end
 
-    
