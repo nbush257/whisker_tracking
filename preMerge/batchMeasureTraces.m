8c74a0be828c0fe81950ce263b9bd449fa407abe
nbush257 <nbush257@gmail.com>
2017-01-23 11:19:19 -0600

    batchMeasureTraces bugix where n was only called to the first clip. Now called for all clips

--- preMerge/batchMeasureTraces.m
+++ preMerge/batchMeasureTraces.m
@@ -24,8 +24,8 @@ parfor ii = 2:length(d)
     measureStr = sprintf('measure --face %i %i %s %s %s',bp(1),bp(2),direction,d(ii).name,measurementsFileName);
     system(measureStr)
     
-    classifyStr = sprintf('classify %s %s %i %i %s --px2mm .04 --follicle %i -n 1',measurementsFileName,measurementsFileName,bp(1),bp(2),direction,fol);
-    reclassifyStr = sprintf('reclassify %s %s -n 1',measurementsFileName,measurementsFileName);
+    classifyStr = sprintf('classify %s %s %i %i %s --px2mm .04 --follicle %i -n %i',measurementsFileName,measurementsFileName,bp(1),bp(2),direction,fol,n);
+    reclassifyStr = sprintf('reclassify %s %s -n %i',measurementsFileName,measurementsFileName,n);
     
     system(classifyStr);
     system(reclassifyStr);
