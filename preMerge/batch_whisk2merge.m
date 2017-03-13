f3ec8c62167b08778b3717f6f133234c11c71987
nbush257 <nbush257@gmail.com>
2017-03-13 11:32:10 -0500

    comments on a batch method for preprocessing whiskers to go to the merge

--- preMerge/batch_whisk2merge.m
+++ preMerge/batch_whisk2merge.m
@@ -1,12 +1,16 @@
+%% batch_whisk2merge
+% this script runs through all the files in the directory and does
+% preprocess smoothing on them prior to the merge. This gets run after the
+% batchgetBPandMask function
+%%
 clear
+% get all the tracked files in the directory
 d = dir('*tracked.mat');
-d_names = {d.name}
+d_names = {d.name};
 
 for ii = 1:length(d)
-    load(d(ii).name,'*W')
-    d_avi_front = dir([d(ii).name(1:27) '*Front*.avi']);
-    d_avi_top = dir([d(ii).name(1:27) '*Top*.avi']);
-    [tws,fws] = whisk2merge(tW,fW,d_avi_top(1).name,d_avi_front(1).name,[d(ii).name(1:27) 'toMerge.mat'])
-    
+    load(d(ii).name)
+    whisk2merge(tW,fW,avi_top,avi_front,[d(ii).name(1:27) 'toMerge.mat'],mask_struct);
+    clear mask_struct *W avi_* 
 end
 
