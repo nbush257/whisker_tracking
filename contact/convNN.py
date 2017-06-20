59b91e222f83d0bd3671013ccb96a4f5d6976a79
nbush257 <nbush257@gmail.com>
2017-06-20 14:16:57 -0500

    restructured conv model function

--- contact/convNN.py
+++ contact/convNN.py
@@ -3,7 +3,7 @@ from keras.models import Sequential
 import scipy.io.matlab as sio
 import sys
 from keras.models import Sequential
-from keras.layers import Dense,Convolution1D,Dropout,MaxPooling1D,AtrousConv1D,Flatten,AveragePooling1D,UpSampling1D
+from keras.layers import Dense,Convolution1D,Dropout,MaxPooling1D,AtrousConv1D,Flatten,AveragePooling1D,UpSampling1D,Activation
 from keras.layers.recurrent import LSTM
 from keras.callbacks import EarlyStopping
 from keras.regularizers import l2,l1
@@ -14,6 +14,41 @@ import scipy.signal as signal
 import sys
 from keras.layers.wrappers import Bidirectional
 
+def create_basic_conv_model(x):
+    model = Sequential()
+    model.add(Convolution1D(8,64,input_shape=x.shape[1:3]))
+    model.add(Activation('relu'))
+    model.add(Convolution1D(8,64,input_shape=x.shape[1:3]))
+    model.add(Activation('relu'))
+    model.add(MaxPooling1D(3,1))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution1D(16,32,input_shape=x.shape[1:3]))
+    model.add(Activation('relu'))
+    model.add(Convolution1D(16,32,input_shape=x.shape[1:3]))
+    model.add(Activation('relu'))
+    model.add(MaxPooling1D(3,1))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution1D(32,16,input_shape=x.shape[1:3]))
+    model.add(Activation('relu'))
+    model.add(Convolution1D(32,16,input_shape=x.shape[1:3]))
+    model.add(Activation('relu'))
+    model.add(MaxPooling1D(3,1))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+
+    model.compile(loss='binary_crossentropy',optimizer='rmsprop',metrics=['accuracy'])
+    model.fit(X,C,epochs=5,batch_size=32,validation_split=0.33)
+    return model
+
 
 def make_conv_mdl(X, 
     filter_length=8,
@@ -22,174 +57,79 @@ def make_conv_mdl(X,
     nb_filter=16,
     nb_fully_connected=256,
     drop_pct=0.2,
-    pool_size=3,
+    pool_size=3
     ):
     
     input_shape = X.shape[1:3]
     model = Sequential()
 
-    # for ii in xrange(n_layers):
     nb_filter = 16
     filter_length=64
-    model.add(
-        Convolution1D(
-            filters=nb_filter,
-            kernel_size=filter_length, 
-            padding='same',
-            input_shape=input_shape,
-            kernel_regularizer=l2(penalty)
-        )
-    )
-    
-    model.add(
-        Convolution1D(
-            filters=nb_filter,
-            kernel_size=filter_length, 
-            padding='same',
-            kernel_regularizer=l2(penalty)
-        )
-    )
-    
-
-    model.add(
-        MaxPooling1D(
-            pool_size=pool_size,
-            strides=1,
-            padding='same'
-        )
-    )
-# ==================================
+    model.add(Convolution1D(nb_filter,filter_length,input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(Convolution1D(nb_filter,filter_length))
+    model.add(Activation('relu'))
+    model.add(MaxPooling1D(pool_size,1))
+    model.add(Dropout(drop_pct))
+    # ==================================
     nb_filter = 32
     filter_length=32
-    model.add(
-        Convolution1D(
-            filters=nb_filter,
-            kernel_size=filter_length, 
-            padding='same',
-            kernel_regularizer=l2(penalty)
-        )
-    )
-    
-    model.add(
-        Convolution1D(
-            filters=nb_filter,
-            kernel_size=filter_length, 
-            padding='same',
-            kernel_regularizer=l2(penalty)
-        )
-    )
-    
-
-    model.add(
-        MaxPooling1D(
-            pool_size=pool_size,
-            strides=1,
-            padding='same'
-        )
-    )
-# ==================================
+    model.add(Convolution1D(nb_filter,filter_length))
+    model.add(Activation('relu'))
+    model.add(Convolution1D(nb_filter,filter_length))
+    model.add(Activation('relu'))
+    model.add(MaxPooling1D(pool_size,1))
+    model.add(Dropout(drop_pct))
+    # ==================================
     nb_filter = 64
     filter_length = 16
-    model.add(
-        Convolution1D(
-            filters=nb_filter,
-            kernel_size=filter_length, 
-            padding='same',
-            kernel_regularizer=l2(penalty)
-        )
-    )
-    
-    model.add(
-        Convolution1D(
-            filters=nb_filter,
-            kernel_size=filter_length, 
-            padding='same',
-            kernel_regularizer=l2(penalty)
-        )
-    )
-    
+    model.add(Convolution1D(nb_filter,filter_length))
+    model.add(Activation('relu'))
+    model.add(Convolution1D(nb_filter,filter_length))
+    model.add(Activation('relu'))
+    model.add(MaxPooling1D(pool_size,1))
+    model.add(Dropout(drop_pct))
+    # ==================================
 
-    model.add(
-        MaxPooling1D(
-            pool_size=pool_size,
-            strides=1,
-            padding='same'
-        )
-    )
-# ==================================
-
-    nb_filter = 128
+    nb_filter = 128    
     filter_length = 8
-    model.add(
-        Convolution1D(
-            filters=nb_filter,
-            kernel_size=filter_length, 
-            padding='same',
-            kernel_regularizer=l2(penalty)
-        )
-    )
-    
-    model.add(
-        Convolution1D(
-            filters=nb_filter,
-            kernel_size=filter_length, 
-            padding='same',
-            kernel_regularizer=l2(penalty)
-        )
-    )
-    
+    model.add(Convolution1D(nb_filter,filter_length))
+    model.add(Activation('relu'))
+    model.add(Convolution1D(nb_filter,filter_length))
+    model.add(Activation('relu'))
+    model.add(MaxPooling1D(pool_size,1))
+    model.add(Dropout(drop_pct))
 
-    model.add(
-        MaxPooling1D(
-            pool_size=pool_size,
-            strides=1,
-            padding='same'
-        )
-    )
-# ===========================
+    # ===========================
     nb_filter = 256
     filter_length = 4
-    model.add(
-        Convolution1D(
-            filters=nb_filter,
-            kernel_size=filter_length, 
-            padding='same',
-            kernel_regularizer=l2(penalty)
-        )
-    )
-    
-    model.add(
-        Convolution1D(
-            filters=nb_filter,
-            kernel_size=filter_length, 
-            padding='same',
-            kernel_regularizer=l2(penalty)
-        )
-    )
-    
-
-    model.add(
-        MaxPooling1D(
-            pool_size=pool_size,
-            strides=1,
-            padding='same'
-        )
-    )
-# ==================================
-
-    model.add(Dense(nb_fully_connected,activation='relu'))
+    model.add(Convolution1D(nb_filter,filter_length))
+    model.add(Activation('relu'))
+    model.add(Convolution1D(nb_filter,filter_length))
+    model.add(Activation('relu'))
+    model.add(MaxPooling1D(pool_size,1))
     model.add(Dropout(drop_pct))
+    # ==================================
 
-    model.add(Dense(nb_fully_connected,activation='relu'))
-    model.add(Dropout(drop_pct))
+    model.add(Flatten())
+    model.add(Dense(nb_fully_connected))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
 
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
     # model.compile(loss='mse', optimizer='adam', metrics=['mae'])
     # To perform (binary) classification instead:
     model.compile(loss='binary_crossentropy',
                   optimizer='rmsprop',
                   metrics=['accuracy'])
     return model
+def make_merge_mdl(X)
+    num_feats = X.shape[-1]
+    for ii in xrange(num_feats):
+        x = X[:,:,ii]
+        model[ii] =create_basic_conv_model(x)
+
 
 def make_tensor(timeseries, window_size=16):
     X = np.empty((timeseries.shape[0],window_size*2,timeseries.shape[-1]))
@@ -203,7 +143,7 @@ def make_tensor_RNN(timeseries,window_size=16):
         X[ii,:,:] = timeseries[ii-window_size:ii,:]
     return X
 
-def makeRNN(X,batch_size,n_layers=5,nb_units=64,penalty=0.,drop_pct=0.,pool_size=3):
+def make_RNN(X,batch_size,n_layers=5,nb_units=64,penalty=0.,drop_pct=0.,pool_size=3):
     input_shape = X.shape[1:3]
     model = Sequential()
 
@@ -250,7 +190,6 @@ def makeRNN(X,batch_size,n_layers=5,nb_units=64,penalty=0.,drop_pct=0.,pool_size
                   metrics=['accuracy'])
     return model
 
-
 def fit(X,C,mdl,epochs=2,batch_size=128,validation_split=0.3):
     C = make_tensor(C,X.shape[1]/2)
     # if (valid_X is None) != (valid_y is None):
@@ -261,19 +200,17 @@ def fit(X,C,mdl,epochs=2,batch_size=128,validation_split=0.3):
     mdl.fit(X,C,
         epochs=epochs,
         batch_size=batch_size,
-        validation_split=validation_split,
-        shuffle=False
-        )
+        validation_split=validation_split)
     return mdl
 
-def cleanVar(x):
+def clean_var(x):
     imp = Imputer()
     x = imp.fit_transform(x)
     x = detrend(x,axis=0)
     x = scale(x)
     return x
 
-def saveNN(model,save_file='model'):
+def save_NN(model,save_file='model'):
     model_json = model.to_json()
     with open(save_file + ".json", "w") as json_file:
         json_file.write(model_json)
@@ -281,7 +218,7 @@ def saveNN(model,save_file='model'):
     model.save_weights(save_file + ".h5")
     print("Saved model to disk")
 
-def loadModel(model_fid):
+def load_model(model_fid):
     json_file = open(model_fid+'.json','r')
     loaded_json = json_file.read()
     json_file.close()
@@ -313,8 +250,6 @@ def main():
     print('\nSaving Data to .mat file\n')
     sio.savemat(fname,data,oned_as='column')
     print('C Vector Saved\n')
-
-    
 
 if __name__ == '__main__':
     main()
