0cfa93e5ad4e6829b8fd7360f2e08357f1f328de
nbush257 <nbush257@gmail.com>
2017-08-29 15:18:51 -0500

    lots of updates to the development of the motor preprocessing. All code are still in development.

--- motor/name_cleaning.py
+++ motor/name_cleaning.py
@@ -4,21 +4,54 @@
 
 import os
 import re
+import shutil
+import sqlite3 as sql
+MOVE_FLG = True
+
 main_dir = r'J:\motor_experiment\video_data'
 good_dir = os.path.join(main_dir,'_good')
 bad_dir = os.path.join(main_dir,'_bad')
 unfinished_dir = os.path.join(main_dir,'_unfinished')
 
-for root,dirs,files in os.walk(good_dir):
+cur_dir = unfinished_dir
+quality = 'unfinished'
+
+
+connection = sql.connect(os.path.join(main_dir,'motor.db'))
+with connection:
+    cur = connection.cursor()
+    cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    if len(cur.fetchall())==0:
+        cur.execute('CREATE TABLE data(ID,whisker,trial_type,direction,modifier,trial_idx,view,fname,ext,quality)')
+    else:
+        print("Table 'data' found in motor.db")
+
+    cur.execute("SELECT MAX(ID) FROM data")
+    ID_val = cur.fetchall()[0][0]
+    if ID_val ==None:
+        ID = 0
+    else:
+        ID = ID_val+1
+
+for root,dirs,files in os.walk(cur_dir):
     for file in files:
+        print file
+        # init
+
+        modifier = ''
+        direction = ''
+        trial_type = ''
+        whisker = ''
+        trial_idx = ''
+
         ext = os.path.splitext(file)[1]
+
         whisker = re.search('(?<![A-Z])[A-Z]{1,2}\d',file).group().upper().strip('_')
 
         # get trial type
-        trial_type = re.search('(?i)collision|(?i)sinewave|(?i)ping',file).group().lower()
-
+        # trial_type = re.search('(?i)collision|(?i)sinewave|(?i)ping',file).group().lower()
         # get direction if it exists
-        if trial_type == 'ping' or trial_type=='collision':
+        if  trial_type=='collision':
             direction = re.search('(?i)neg|(?i)pos',file).group()
             # get modifier
             token = '(?<={}).*(?={})'.format(direction, whisker)
@@ -46,13 +79,20 @@ for root,dirs,files in os.walk(good_dir):
             whisker = 'R' + whisker
         # get trial index
         trial_idx = re.search('t\d{2}',file).group().lower()
+        trial_idx_num = int(trial_idx[1:])
 
         view = re.search('(?i)front|(?i)top',file).group().lower()
 
-        newname= '_'.join(['motor',trial_type,direction,whisker,'e'+modifier.zfill(2),trial_idx,view+ext])
+        newname= '_'.join(['motor',whisker,trial_type,direction,'e'+modifier.zfill(4),trial_idx,view+ext])
+        newname=re.sub('_+', '_', newname)
+        if MOVE_FLG:
+            if not os.path.isfile(os.path.join(cur_dir,newname)):
+                shutil.move(os.path.join(cur_dir,file),os.path.join(cur_dir,newname))
 
-        print file
-        print newname
-        print '\n'
+        with connection:
+            cur = connection.cursor()
+            cur.execute('INSERT INTO data VALUES (?,?,?,?,?,?,?,?,?,?)',(ID,whisker,trial_type,direction,modifier,trial_idx_num,view,newname,ext,quality))
+            ID +=1
+
 
 
