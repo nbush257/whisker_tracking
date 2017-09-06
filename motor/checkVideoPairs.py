d9f91cb112fe40f21c0e73abbb2175e6b0c1aa58
nbush257 <nbush257@gmail.com>
2017-09-06 10:25:57 -0500

    check pairs uses whisker files to determine if the number of frames is equivalent in each view. This avoids the problem of not being able to move an open video file. Still needs sanity checks

--- motor/checkVideoPairs.py
+++ motor/checkVideoPairs.py
@@ -1,15 +1,16 @@
-import pims
 import glob
 import shutil
 import os
 import sys
 import re
-
+sys.path.append(r'L:\Users\guru\Documents\hartmann_lab\proc\whisk\python')
+from trace_2 import Load_Whiskers,Save_Whiskers
 def checkPairs(w_path) :
-    d_front = glob.glob(os.path.join(w_path,'*front.avi'))
-    d_top = glob.glob(os.path.join(w_path ,'*top.avi'))
-    # d_front_whiskers = glob.glob(w_path + '\*Front.whiskers')
-    # d_top_whiskers = glob.glob(w_path + '\*Top.whiskers')
+
+    # d_front = glob.glob(os.path.join(w_path,'*front.avi'))
+    # d_top = glob.glob(os.path.join(w_path ,'*top.avi'))
+    d_front_whiskers = glob.glob(w_path + '\*front_labelled.whiskers')
+    d_top_whiskers = glob.glob(w_path + '\*top_labelled.whiskers')
 
     mismatch = os.path.join(w_path, 'mismatch')
     paired = os.path.join(w_path, 'paired_and_matched')
@@ -18,36 +19,36 @@ def checkPairs(w_path) :
     if not os.path.isdir(paired):
         os.mkdir(paired)
 
-    for file_front in d_front:
-        token = re.split('front.avi',file_front)[0]
-        file_top = token + 'top.avi'
-        file_front_whiskers = os.path.splitext(file_front)[0] + '.whiskers'
-        file_top_whiskers = os.path.splitext(file_top)[0] + '.whiskers'
+    for file_front in d_front_whiskers:
+        token = re.split('front_labelled.whiskers',file_front)[0]
+        print('Working on {}'.format(token[:-1]))
+        file_top = token + 'top_labelled.whiskers'
+        file_front_video = token+'front.avi'
+        file_top_video = token+'top.avi'
 
         # if there is no corresponding view, then the file remains in the root folder (if no front but a top, does not get checked for, if no top but front, passes
-        if file_top in d_top:
-            V_front = pims.open(file_front)
-            V_top = pims.open(file_top)
-            l_front = len(V_front)
-            l_top = len(V_top)
-            V_front.close()
-            V_top.close()
+        if file_top in d_top_whiskers:
+            w_top = Load_Whiskers(file_top)
+            w_front = Load_Whiskers(file_front)
+            l_top = max(w_top.keys())
+            l_front = max(w_front.keys())
+
             if l_front != l_top:
                 # move to mismatch if there are an uneven number of frames
                 shutil.move(file_front, mismatch)
                 shutil.move(file_top, mismatch)
-                if os.isfile(file_front_whiskers):
-                    shutil.move(file_front_whiskers, mismatch)
-                if os.isfile(file_top_whiskers):
-                    shutil.move(file_top_whiskers, mismatch)
+                if os.path.isfile(file_front_video):
+                    shutil.move(file_front_video, mismatch)
+                if os.path.isfile(file_top_video):
+                    shutil.move(file_top_video, mismatch)
             else:
                 # this is the success case
                 shutil.move(file_front, paired)
                 shutil.move(file_top, paired)
-                if os.path.isfile(file_front_whiskers):
-                    shutil.move(file_front_whiskers, paired)
-                if os.path.isfile(file_top_whiskers):
-                    shutil.move(file_top_whiskers, paired)
+                if os.path.isfile(file_front_video):
+                    shutil.move(file_front_video, paired)
+                if os.path.isfile(file_top_video):
+                    shutil.move(file_top_video, paired)
         else:
             pass
 
