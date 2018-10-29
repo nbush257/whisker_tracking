e7b425cdff8cd03f40e8b3040aff1394715e5730
nbush257 <nbush257@gmail.com>
2018-10-29 18:28:20 -0500

    added contact model

--- contact/applyContactModel.py
+++ contact/applyContactModel.py
@@ -3,9 +3,13 @@ import scipy.io.matlab as sio
 import glob
 import numpy as np
 import sys
-
+import os
+import re
 def applyModel(mdl,data_fname):
 	data = sio.loadmat(data_fname)
+	root = re.search('rat\d{4}_\d\d_[A-Z]{3}\d\d_VG_[A-Z]\d_t\d\d',data_fname).group()
+	p = os.path.split(data_fname)[0]
+	data_out_name = os.path.join(p,root + '_contact2D.mat')
 	X = data['X'][:,:4]
 	C = np.zeros(X.shape[0],dtype='bool')
 	window_size = mdl.layers[0].input_shape[1]/2
@@ -13,7 +17,7 @@ def applyModel(mdl,data_fname):
 	C = mdl.predict_classes(XX)
 	C = C.astype('bool')
 	data_struct = {'C':C,'X':X,'tip':data['tip']}
-	sio.savemat(data_fname,data_struct)
+	sio.savemat(data_out_name,data_struct)
 
 if __name__ == '__main__':
 	model_name = sys.argv[1] # model is the first argument
